84cda7ba96901ed182082c060504cca640c8c7a5
Mike Allaway <m.j.allaway@bham.ac.uk>
2020-10-27 20:55:42 +0000

    updated model based on meeting with jeremy

--- django/api/admin.py
+++ django/api/admin.py
@@ -15,12 +15,22 @@ class ProjectAdminView(admin.ModelAdmin):
     ordering = ('-meta_lastupdated_datetime', 'name')
 
 
+class JournalEntryTagAdminView(admin.ModelAdmin):
+    """
+    Customise the Journal Entry Tag section of the Django admin
+    """
+    list_display = ('name', 'description', 'is_public', 'user', 'admin_published', 'meta_lastupdated_datetime')
+    list_filter = ('admin_published', 'is_public', 'user')
+    search_fields = ('name', 'description')
+    ordering = ('-meta_lastupdated_datetime', 'name')
+
+
 class JournalEntryAdminView(admin.ModelAdmin):
     """
     Customise the Journal Entry section of the Django admin
     """
-    list_display = ('title', 'entry_text', 'project', 'user', 'admin_published', 'meta_lastupdated_datetime')
-    list_filter = ('admin_published', 'project', 'user')
+    list_display = ('title', 'entry_text', 'user', 'admin_published', 'meta_lastupdated_datetime')
+    list_filter = ('admin_published', 'user')
     search_fields = ('title', 'entry_text')
     ordering = ('-meta_lastupdated_datetime', 'title')
 
@@ -35,7 +45,30 @@ class JournalEntryImageAdminView(admin.ModelAdmin):
     ordering = ('-meta_lastupdated_datetime', 'name')
 
 
+class JournalEntryAnalysisCodeAdminView(admin.ModelAdmin):
+    """
+    Customise the Journal Entry Analysis Code section of the Django admin
+    """
+    list_display = ('name', 'description', 'admin_published', 'meta_lastupdated_datetime')
+    list_filter = ('admin_published',)
+    search_fields = ('name', 'description')
+    ordering = ('-meta_lastupdated_datetime', 'name')
+
+
+class JournalEntryAnalysisAdminView(admin.ModelAdmin):
+    """
+    Customise the Journal Entry Analysis section of the Django admin
+    """
+    list_display = ('analysis_text', 'journal_entry', 'admin_published', 'meta_lastupdated_datetime')
+    list_filter = ('admin_published',)
+    search_fields = ('analysis_text',)
+    ordering = ('-meta_lastupdated_datetime', 'analysis_text')
+
+
 # Register classes
 admin.site.register(models.Project, ProjectAdminView)
+admin.site.register(models.JournalEntryTag, JournalEntryTagAdminView)
 admin.site.register(models.JournalEntry, JournalEntryAdminView)
 admin.site.register(models.JournalEntryImage, JournalEntryImageAdminView)
+admin.site.register(models.JournalEntryAnalysisCode, JournalEntryAnalysisCodeAdminView)
+admin.site.register(models.JournalEntryAnalysis, JournalEntryAnalysisAdminView)
