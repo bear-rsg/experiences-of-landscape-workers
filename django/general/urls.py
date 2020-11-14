076cf1096fbeedaea729b17bbd2dfba30b4f0f83
Mike Allaway <m.j.allaway@bham.ac.uk>
2020-11-14 18:23:56 +0000

    removed a lot of the content to add back in another MR, to keep this one small as it got very large

--- django/general/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('cookies/', views.CookiesTemplateView.as_view(), name='cookies'),
-]
--- django/general/views.py
+++
@@ -1,8 +0,0 @@
-from django.views.generic import TemplateView
-
-
-class CookiesTemplateView(TemplateView):
-    """
-    Class-based view to show the cookies template
-    """
-    template_name = 'general/cookies.html'
