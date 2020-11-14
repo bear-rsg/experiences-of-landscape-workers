ebfdbba6704817b13e774a68d4da8cd5198425e2
Mike Allaway <m.j.allaway@bham.ac.uk>
2020-11-14 18:30:39 +0000

    removed general urls

--- django/core/urls.py
+++ django/core/urls.py
@@ -8,9 +8,6 @@ urlpatterns = [
     # Account app's urls
     path('', include('accounts.urls')),
 
-    # General app's urls
-    path('', include('general.urls')),
-
     # Journal app's urls
     path('journal/', include('journal.urls')),
 
