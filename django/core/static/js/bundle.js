3102574971aa6d06ed38d60c3ae2060d94ee9f15
Michael Allaway <m.j.allaway@bham.ac.uk>
2021-02-05 13:50:32 +0000

    a large chunk of front end css and html

--- django/core/static/js/bundle.js
+++ django/core/static/js/bundle.js
@@ -31,33 +31,4 @@ var getUrlParameter = require('./getUrlParameter');
 // Initiate jQuery
 $(document).ready(function() {
 
-    // Navigation
-
-    // Navigation toggle (on small displays)
-    $('#nav-toggle').click(function(){
-        if($('#nav-toggle').hasClass('nav-visible')) $('#nav-toggle, nav').removeClass('nav-visible');
-        else $('#nav-toggle, nav').addClass('nav-visible');
-    });
-
-    // Hide the nav menu when a link within it is clicked (for small displays)
-    $('nav ul li a').click(function(){
-        $('#nav-toggle').trigger('click');
-    });
-
-    // Remove empty columns in a table
-    $('table tr th').each(function(i) {
-        //select all tds in this column
-        var tds = $(this).parents('table')
-                .find('tr td:nth-child(' + (i + 1) + ')');
-        //check if all the cells in this column are empty
-        if(tds.length == tds.filter(':empty').length) { 
-            //hide header
-            $(this).hide();
-            //hide cells
-            tds.hide();
-        } 
-    });
-
-});
-
 },{"./getUrlParameter":1}]},{},[2]);
