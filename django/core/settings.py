d555a71dce8ddd1e3641ea68bf9704b9e13a102e
Michael Allaway <m.j.allaway@bham.ac.uk>
2021-02-10 13:58:38 +0000

    tabs to spaces

--- django/core/settings.py
+++ django/core/settings.py
@@ -69,22 +69,22 @@ PWA_APP_ORIENTATION = 'any'
 PWA_APP_START_URL = '/'
 PWA_APP_STATUS_BAR_COLOR = 'default'
 PWA_APP_ICONS = [
-	{
-		'src': 'static/images/pwa/icon-160.png',
-		'sizes': '160x160'
-	}
+    {
+        'src': 'static/images/pwa/icon-160.png',
+        'sizes': '160x160'
+    }
 ]
 PWA_APP_ICONS_APPLE = [
-	{
-		'src': 'static/images/pwa/icon-160.png',
-		'sizes': '160x160'
-	}
+    {
+        'src': 'static/images/pwa/icon-160.png',
+        'sizes': '160x160'
+    }
 ]
 PWA_APP_SPLASH_SCREEN = [
-	{
-		'src': 'static/images/pwa/icon-160.png',
-		'media': '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)'
-	}
+    {
+        'src': 'static/images/pwa/icon-160.png',
+        'media': '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)'
+    }
 ]
 PWA_APP_DIR = 'ltr'
 PWA_APP_LANG = 'en-US'
