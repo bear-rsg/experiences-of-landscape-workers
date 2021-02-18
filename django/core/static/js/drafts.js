c971843013618f4dcb21a8b4e99b42cb5b3ffbb6
Michael Allaway <m.j.allaway@bham.ac.uk>
2021-02-18 12:13:20 +0000

    linted drafts.js

--- django/core/static/js/drafts.js
+++ django/core/static/js/drafts.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-    
     //
     // Cookie functions: get, set, delete
     //
@@ -15,7 +14,7 @@ $(document).ready(function () {
         // Loop through cookies list to find a match
         for (i = 0; i < c_list.length; i++) {
             // Find cookie
-            c = c_list[i].split('=');
+            c = c_list[i].split("=");
             c_name = c[0];
             c_value = c[1];
             // Return cookie value if cookie name matches
@@ -34,9 +33,9 @@ $(document).ready(function () {
 
     // Delete cookie by name
     function deleteCookie(name) {
-        document.cookie = name + "=0; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        document.cookie =
+            name + "=0; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     }
-
 
     //
     // General Functions
@@ -54,8 +53,7 @@ $(document).ready(function () {
             : string.substring(0, length) + "...";
     }
 
-
-    // 
+    //
     // Drafts
     //
 
@@ -67,72 +65,92 @@ $(document).ready(function () {
     });
 
     // Load current draft in 'create journal entry' form
-    $("#drafteditor-buttons").on("click", "#drafteditor-buttons-addtojournal", function () {
-        // Must be editing an existing draft and be online
-        if (navigator.onLine) {
-            if (getCookie("currentDraft")) {
-                // Get cleaned text
-                var txt = $.trim($("#drafteditor-text").val());
-                // Save progress of current draft
-                db.drafts
-                    .put({ name: getCookie("currentDraft"), text: txt })
-                    .then(function () {
-                        return db.drafts.get(getCookie("currentDraft"));
-                    })
-                    .then(function (draft) {
-                        // Set currentDraft cookie
-                        setCookie('currentDraft', draft.name);
-                        // Go to journal
-                        window.location.assign("/journal/create/");
-                    });
-            }
-            else {
-                alert('Cannot load draft. Please try saving your draft and restarting this app.')
+    $("#drafteditor-buttons").on(
+        "click",
+        "#drafteditor-buttons-addtojournal",
+        function () {
+            // Must be editing an existing draft and be online
+            if (navigator.onLine) {
+                if (getCookie("currentDraft")) {
+                    // Get cleaned text
+                    var txt = $.trim($("#drafteditor-text").val());
+                    // Save progress of current draft
+                    db.drafts
+                        .put({ name: getCookie("currentDraft"), text: txt })
+                        .then(function () {
+                            return db.drafts.get(getCookie("currentDraft"));
+                        })
+                        .then(function (draft) {
+                            // Set currentDraft cookie
+                            setCookie("currentDraft", draft.name);
+                            // Go to journal
+                            window.location.assign("/journal/create/");
+                        });
+                } else {
+                    alert(
+                        "Cannot load draft. Please try saving your draft and restarting this app."
+                    );
+                }
+            } else {
+                alert(
+                    "Oops, it looks like you're offline. Please try again when you have an internet connection."
+                );
             }
         }
-        else {
-            alert("Oops, it looks like you're offline. Please try again when you have an internet connection.")
-        }
-    });
+    );
 
     // Save current draft
-    $("#drafteditor-buttons").on("click", "#drafteditor-buttons-save", function () {
-        dt = Date.now();
-        var txt = $.trim($("#drafteditor-text").val());
+    $("#drafteditor-buttons").on(
+        "click",
+        "#drafteditor-buttons-save",
+        function () {
+            dt = Date.now();
+            var txt = $.trim($("#drafteditor-text").val());
 
-        // Draft text can't be empty when saving
-        if (txt !== "") {
-            // If a draft is currently being edited, update it. Otherwise add new draft using current timestamp
-            var draftname = getCookie("currentDraft") ? getCookie("currentDraft") : String(dt);
+            // Draft text can't be empty when saving
+            if (txt !== "") {
+                // If a draft is currently being edited, update it. Otherwise add new draft using current timestamp
+                var draftname = getCookie("currentDraft")
+                    ? getCookie("currentDraft")
+                    : String(dt);
 
-            // Save draft (automatically knows whether to update/add new based on matching name)
-            db.drafts
-                .put({ name: draftname, text: txt })
-                .then(function () {
-                    return db.drafts.get(draftname);
-                })
-                .then(function (draft) {
-                    // Delete currentDraft cookie
-                    deleteCookie('currentDraft');
-                    // Refresh page
-                    window.location.assign(window.location.pathname + "#mydrafts-title");
-                });
-        } else {
-            alert("Draft is empty");
+                // Save draft (automatically knows whether to update/add new based on matching name)
+                db.drafts
+                    .put({ name: draftname, text: txt })
+                    .then(function () {
+                        return db.drafts.get(draftname);
+                    })
+                    .then(function (draft) {
+                        // Delete currentDraft cookie
+                        deleteCookie("currentDraft");
+                        // Refresh page
+                        window.location.assign(
+                            window.location.pathname + "#mydrafts-title"
+                        );
+                    });
+            } else {
+                alert("Draft is empty");
+            }
         }
-    });
+    );
 
     // Delete current draft
-    $("#drafteditor-buttons").on("click", "#drafteditor-buttons-delete", function () {
-        if (getCookie("currentDraft")) {
-            db.drafts.delete(getCookie("currentDraft")).then(function (currentDraft) {
-                // Delete currentDraft cookie
-                deleteCookie('currentDraft');
-                // Refresh page
-                window.location.assign(window.location.pathname);
-            });
+    $("#drafteditor-buttons").on(
+        "click",
+        "#drafteditor-buttons-delete",
+        function () {
+            if (getCookie("currentDraft")) {
+                db.drafts
+                    .delete(getCookie("currentDraft"))
+                    .then(function (currentDraft) {
+                        // Delete currentDraft cookie
+                        deleteCookie("currentDraft");
+                        // Refresh page
+                        window.location.assign(window.location.pathname);
+                    });
+            }
         }
-    });
+    );
 
     // Delete current draft when submitting journal entry
     $("#journalentry-create-form").on("submit", function () {
@@ -151,9 +169,13 @@ $(document).ready(function () {
                 .each(function (draft) {
                     items +=
                         `<div class="drafts-list-item">
-                        <a href="#drafteditor-title" class="drafts-list-item-link" id="` + draft.name + `">`
-                        + tidyDate(draft.name) + `</a><p>`
-                        + truncate(draft.text, 30) + `</p></div>`;
+                                                <a href="#drafteditor-title" class="drafts-list-item-link" id="` +
+                        draft.name +
+                        `">` +
+                        tidyDate(draft.name) +
+                        `</a><p>` +
+                        truncate(draft.text, 30) +
+                        `</p></div>`;
                 })
                 .then(function () {
                     html = `<div id="drafts-list">` + items + `</div>`;
@@ -163,15 +185,14 @@ $(document).ready(function () {
     });
 
     // Clear currentDraft cookie each time a page is navigated to via the navigation buttons
-    $('.nav-item').on('click', function () {
-        deleteCookie('currentDraft');
+    $(".nav-item").on("click", function () {
+        deleteCookie("currentDraft");
     });
 
     // Clicking on a draft in the list
-    $('#mydrafts').on('click', '.drafts-list-item-link', function () {
-        
+    $("#mydrafts").on("click", ".drafts-list-item-link", function () {
         // Set this draft (based on its id) as the currentDraft
-        setCookie('currentDraft', $(this).prop('id'))
+        setCookie("currentDraft", $(this).prop("id"));
 
         // Load current draft text into text editor
         if (getCookie("currentDraft")) {
@@ -184,15 +205,18 @@ $(document).ready(function () {
 
     // Load current draft text into text area
     if (getCookie("currentDraft")) {
-            db.drafts.get(getCookie("currentDraft")).then(function (currentDraft) {
+        db.drafts
+            .get(getCookie("currentDraft"))
+            .then(function (currentDraft) {
                 $("#id_entry_text, #drafteditor-text").val(currentDraft.text);
-            }).catch(function(error) {
+            })
+            .catch(function (error) {
                 console.log("Expected error: " + error);
             });
     }
 
     // Show pending drafts alert to user
-    var pagesToExcludeAlerts = ['/journal/drafts/', '/journal/create/'];
+    var pagesToExcludeAlerts = ["/journal/drafts/", "/journal/create/"];
     var currentUrl = window.location.pathname;
     // If online and current URL isn't in above exclude list
     if (navigator.onLine && !pagesToExcludeAlerts.includes(currentUrl)) {
@@ -211,10 +235,11 @@ $(document).ready(function () {
 
         // Change text label
         // If a currentDraft cookie exists, load it into
-        if (getCookie("currentDraft")) var label = "Edit draft:- " + tidyDate(getCookie("currentDraft"));
-        else var label = 'Create a draft:';
+        if (getCookie("currentDraft"))
+            var label = "Edit draft:- " + tidyDate(getCookie("currentDraft"));
+        else var label = "Create a draft:";
         // Apply HTML
-        $('#drafteditor-text-label').html(label)
+        $("#drafteditor-text-label").html(label);
 
         // Change the action buttons
         if (getCookie("currentDraft")) {
@@ -224,14 +249,12 @@ $(document).ready(function () {
                 buttons += `<button id="drafteditor-buttons-addtojournal" class="btn btn-primary"><i class="fas fa-plus-circle"></i> Add To Journal</button>`;
             }
             buttons += `<button id="drafteditor-buttons-delete" class="btn btn-danger"><i class="fas fa-trash-alt"></i> Delete</button>`;
-        }
-        else {
+        } else {
             var buttons = `<button id="drafteditor-buttons-save" type="submit" class="btn btn-primary"><i class="fas fa-pen"></i> Create Draft</button>`;
         }
         // Apply HTML
-        $('#drafteditor-buttons').html(buttons);
+        $("#drafteditor-buttons").html(buttons);
     }
 
     setDraftEditorState();
-
 });
