50cbddb96e4c63ba3436e39ddf5084628aaffa9f
Michael Allaway <m.j.allaway@bham.ac.uk>
2021-02-21 14:50:43 +0000

    set loading to appear on update form as well

---
+++ django/core/static/js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+
+    // Submit journal entry form
+    $('#journalentry-form-submit').on('click', function(e) {                
+        // If form mandatory text is present
+        if ($("#id_entry_text").val() != '') {
+            // Stop form from submitting
+            e.preventDefault();
+            // Show loading screen
+            $('.loading-container').show();
+            $('html, body').css('overflow', 'hidden');
+            // Articificially add a second for the loading screen
+            setTimeout(function () {
+                $('#journalentry-form').submit();    
+            }, 1000);
+        }
+        else {
+            alert("Journal entry text is empty");
+        }
+    });
+
+});
