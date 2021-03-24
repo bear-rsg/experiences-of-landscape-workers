c1ba2d51238f3a73935d045940233bcc2e674e53
Michael Allaway <m.j.allaway@bham.ac.uk>
2021-03-24 22:52:31 +0000

    added a check for internet connection when submitting journal entry form

--- django/core/static/js/main.js
+++ django/core/static/js/main.js
@@ -6,14 +6,25 @@ $(document).ready(function () {
         if ($("#id_entry_text").val() != '') {
             // Stop form from submitting
             e.preventDefault();
-            // Show loading screen
-            $('.loading-container').show();
-            $('html, body').css('overflow', 'hidden');
-            // Articificially add a second for the loading screen
-            setTimeout(function () {
-                $('#journalentry-form').submit();    
-            }, 1000);
+
+            // If can connect to the internet
+            if(navigator.onLine) {
+                // Show loading screen
+                $('.loading-container').show();
+                $('html, body').css('overflow', 'hidden');
+                // Articificially add a split second for the loading screen
+                setTimeout(function () {
+                    $('#journalentry-form').submit();    
+                }, 640);
+            }
+            // If cannot connect to the internet
+            else {
+                alert(`No internet connection found.
+
+Please try again when you have a connection or in the meantime, you can save this journal entry as a Draft`);
+            }
         }
+        // If manadatory text is not present
         else {
             alert(`You need to supply text for each journal entry you upload.
             
